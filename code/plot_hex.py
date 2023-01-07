fcfcbd24400264d03b797cb5c486cee96e515580
LucijaZuzic <lzuzic49@gmail.com>
2023-01-07 16:09:41 +0100

    Hexa seed color

--- code/plot_hex.py
+++ code/plot_hex.py
@@ -15,6 +15,9 @@ def hex_predictions_png_name(some_path, number, final_model_type, iteration):
 def hex_predictions_final_name(some_path):
     return "../seeds/all_seeds/" + PATH_TO_EXTENSION[some_path] + "_all_tests_hex_predictions.png"
 
+def hex_predictions_seed_name(some_path):
+    return "../seeds/seed_" + str(seed) + some_path.replace("..", "") + PATH_TO_EXTENSION[some_path] + "_seed_" + str(seed) + "_hex_predictions.png"
+
 def myfunc(x):
   return slope * x + intercept
 
@@ -51,16 +54,30 @@ for seed in seed_list:
         for i in df['AP']:
             actual_AP_long.append(i) 
 
+actual_AP_mid = []
+ 
+for number in range(1, NUM_TESTS + 1):
+    for i in df['AP']:
+        actual_AP_mid.append(i) 
+
 model_predictions_seq_hex = []
 model_predictions_AP_hex = []
 model_predictions_hex = []
+
+model_predictions_seq_hex_seed = {}
+model_predictions_AP_hex_seed = {}
+model_predictions_hex_seed = {}
 
 for seed in seed_list:
     setSeed(seed)        
     print("Seed " + str(seed))
 
+    model_predictions_seq_hex_seed[seed] = []
+    model_predictions_AP_hex_seed[seed] = []
+    model_predictions_hex_seed[seed] = []
+
     for number in range(1, NUM_TESTS + 1):
-        print("Test " + str(number))
+        print("Seed " + str(seed) + " Test " + str(number))
 
         model_predictions_seq_hex_file = open(hex_predictions_name(SEQ_MODEL_DATA_PATH, number, 'weak', 1), 'r')
         model_predictions_AP_hex_file = open(hex_predictions_name(MY_MODEL_DATA_PATH, number, 'weak', 1), 'r')
@@ -76,12 +93,15 @@ for seed in seed_list:
 
         for x in model_predictions_seq_hex_one:
             model_predictions_seq_hex.append(x)
+            model_predictions_seq_hex_seed[seed].append(x)
 
         for x in model_predictions_AP_hex_one:
             model_predictions_AP_hex.append(x)
+            model_predictions_AP_hex_seed[seed].append(x)
 
         for x in model_predictions_hex_one:
             model_predictions_hex.append(x)
+            model_predictions_hex_seed[seed].append(x)
 
         plt.figure()
         plt.title(
@@ -154,7 +174,89 @@ for seed in seed_list:
         )  
         plt.savefig(hex_predictions_png_name(MY_MODEL_DATA_PATH, number, 'weak', 1))
         plt.close()
-        
+
+    print("Seed " + str(seed) + " All")
+
+    slope, intercept, r, p, std_err = stats.linregress(model_predictions_seq_hex_seed[seed], actual_AP_mid)
+    print("Seq. model R: " + str(r))
+    print("Seq. model corrcoef: " + str(np.corrcoef(model_predictions_seq_hex_seed[seed], actual_AP_mid)[0][1])) 
+    print("Seq. model spearmanr: " + str(stats.spearmanr(model_predictions_seq_hex_seed[seed], actual_AP_mid)[0])) 
+    print("Seq. model R2: " + str(sklearn.metrics.r2_score(model_predictions_seq_hex_seed[seed], actual_AP_mid)))
+
+    plt.figure()
+    plt.title(
+        merge_type_iteration(SEQ_MODEL_DATA_PATH, 'weak', 1, 0).replace("Test 0 Weak 1", "Seed " + str(seed)) 
+        + "\nPredicted self assembly probability for hexapeptides"
+    )
+    plt.xlabel("Predicted self assembly probability")
+    plt.ylabel("AP") 
+    plt.scatter(
+        model_predictions_seq_hex_seed[seed],
+        actual_AP_mid,
+        label = "Seed " + str(seed)
+    )   
+    mymodel = list(map(myfunc, model_predictions_seq_hex_seed[seed])) 
+    plt.plot(
+        model_predictions_seq_hex_seed[seed],
+        mymodel, color = 'r'
+    )   
+    plt.savefig(hex_predictions_seed_name(SEQ_MODEL_DATA_PATH))
+    plt.close()
+
+    slope, intercept, r, p, std_err = stats.linregress(model_predictions_hex_seed[seed], actual_AP_mid)
+    print("Seq. and AP model R: " + str(r))
+    print("Seq. and AP model corrcoef: " + str(np.corrcoef(model_predictions_hex_seed[seed], actual_AP_mid)[0][1])) 
+    print("Seq. and AP model spearmanr: " + str(stats.spearmanr(model_predictions_hex_seed[seed], actual_AP_mid)[0])) 
+    print("Seq. and AP model R2: " + str(sklearn.metrics.r2_score(model_predictions_hex_seed[seed], actual_AP_mid)))
+
+    plt.figure()
+    plt.title(
+        merge_type_iteration(MODEL_DATA_PATH, 'weak', 1, 0).replace("Test 0 Weak 1", "Seed " + str(seed)) 
+        + "\nPredicted self assembly probability for hexapeptides"
+    )
+    plt.xlabel("Predicted self assembly probability")
+    plt.ylabel("AP") 
+    plt.scatter(
+        model_predictions_hex_seed[seed],
+        actual_AP_mid,
+        label = "Seed " + str(seed)
+    )   
+    mymodel = list(map(myfunc, model_predictions_hex_seed[seed])) 
+    plt.plot(
+        model_predictions_hex_seed[seed],
+        mymodel, color = 'r'
+    )   
+    plt.savefig(hex_predictions_seed_name(MODEL_DATA_PATH))
+    plt.close()
+
+    slope, intercept, r, p, std_err = stats.linregress(model_predictions_AP_hex_seed[seed], actual_AP_mid)
+    print("AP model R: " + str(r))
+    print("AP model corrcoef: " + str(np.corrcoef(model_predictions_AP_hex_seed[seed], actual_AP_mid)[0][1])) 
+    print("AP model spearmanr: " + str(stats.spearmanr(model_predictions_AP_hex_seed[seed], actual_AP_mid)[0])) 
+    print("AP model R2: " + str(sklearn.metrics.r2_score(model_predictions_AP_hex_seed[seed], actual_AP_mid)))
+
+    plt.figure()
+    plt.title(
+        merge_type_iteration(MY_MODEL_DATA_PATH, 'weak', 1, 0).replace("Test 0 Weak 1", "Seed " + str(seed)) 
+        + "\nPredicted self assembly probability for hexapeptides"
+    )
+    plt.xlabel("Predicted self assembly probability")
+    plt.ylabel("AP") 
+    plt.scatter(
+        model_predictions_AP_hex_seed[seed],
+        actual_AP_mid,
+        label = "Seed " + str(seed)
+    )   
+    mymodel = list(map(myfunc, model_predictions_AP_hex_seed[seed])) 
+    plt.plot(
+        model_predictions_AP_hex_seed[seed],
+        mymodel, color = 'r'
+    )   
+    plt.savefig(hex_predictions_seed_name(MY_MODEL_DATA_PATH))
+    plt.close()
+   
+print("All seeds") 
+
 plt.figure()
 plt.title(
     merge_type_iteration(SEQ_MODEL_DATA_PATH, 'weak', 1, 0).replace("Test 0 Weak 1", "All tests") 
@@ -162,10 +264,12 @@ plt.title(
 )
 plt.xlabel("Predicted self assembly probability")
 plt.ylabel("AP")
-plt.scatter(
-    model_predictions_seq_hex,
-    actual_AP_long
-) 
+for seed in seed_list:
+    plt.scatter(
+        model_predictions_seq_hex_seed[seed],
+        actual_AP_mid,
+        label = "Seed " + str(seed)
+    ) 
 slope, intercept, r, p, std_err = stats.linregress(model_predictions_seq_hex, actual_AP_long)
 print("Seq. model R: " + str(r))
 print("Seq. model corrcoef: " + str(np.corrcoef(model_predictions_seq_hex, actual_AP_long)[0][1])) 
@@ -174,8 +278,9 @@ print("Seq. model R2: " + str(sklearn.metrics.r2_score(model_predictions_seq_hex
 mymodel = list(map(myfunc, model_predictions_seq_hex)) 
 plt.plot(
     model_predictions_seq_hex,
-    mymodel, color = 'r'
+    mymodel, color = 'b'
 )  
+plt.legend()
 plt.savefig(hex_predictions_final_name(SEQ_MODEL_DATA_PATH))
 plt.close()
 
@@ -186,10 +291,12 @@ plt.title(
 )
 plt.xlabel("Predicted self assembly probability")
 plt.ylabel("AP")
-plt.scatter(
-    model_predictions_hex,
-    actual_AP_long
-) 
+for seed in seed_list:
+    plt.scatter(
+        model_predictions_hex_seed[seed],
+        actual_AP_mid,
+        label = "Seed " + str(seed)
+    ) 
 slope, intercept, r, p, std_err = stats.linregress(model_predictions_hex, actual_AP_long)
 print("Seq. and AP model R: " + str(r)) 
 print("Seq. and AP model corrcoef: " + str(np.corrcoef(model_predictions_hex, actual_AP_long)[0][1])) 
@@ -198,8 +305,9 @@ print("Seq. and AP model R2: " + str(sklearn.metrics.r2_score(model_predictions_
 mymodel = list(map(myfunc, model_predictions_hex)) 
 plt.plot(
     model_predictions_hex,
-    mymodel, color = 'r'
+    mymodel, color = 'b'
 )  
+plt.legend()
 plt.savefig(hex_predictions_final_name(MODEL_DATA_PATH))
 plt.close() 
 
@@ -210,10 +318,12 @@ plt.title(
 )
 plt.xlabel("Predicted self assembly probability")
 plt.ylabel("AP")
-plt.scatter(
-    model_predictions_AP_hex,
-    actual_AP_long
-) 
+for seed in seed_list:
+    plt.scatter(
+        model_predictions_AP_hex_seed[seed],
+        actual_AP_mid,
+        label = "Seed " + str(seed)
+    ) 
 slope, intercept, r, p, std_err = stats.linregress(model_predictions_AP_hex, actual_AP_long)
 print("AP model R: " + str(r)) 
 print("AP model corrcoef: " + str(np.corrcoef(model_predictions_AP_hex, actual_AP_long)[0][1])) 
@@ -222,7 +332,8 @@ print("AP model R2: " + str(sklearn.metrics.r2_score(model_predictions_AP_hex, a
 mymodel = list(map(myfunc, model_predictions_AP_hex)) 
 plt.plot(
     model_predictions_AP_hex,
-    mymodel, color = 'r'
+    mymodel, color = 'b'
 )  
+plt.legend()
 plt.savefig(hex_predictions_final_name(MY_MODEL_DATA_PATH))
 plt.close()  
